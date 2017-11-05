9148e8fd081a307b262bbc11953655dc91bdb588
Aneta <anetaryznar@gmail.com>
2017-11-05 14:44:00 +0100

    wersja 1

--- src/pl/sdacademy/java7krk/Main.java
+++ src/pl/sdacademy/java7krk/Main.java
@@ -1,8 +1,30 @@
 package pl.sdacademy.java7krk;
 
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Jak masz na imie");
+        String name = scanner.nextLine();
+        String[] names = new String[name.length()];
+
+
+        for (int i = 0; i < names.length; i++) {
+            if (name.endsWith("a")) {
+                System.out.println(name + " jest kobieta");
+                break;
+            } else {
+                System.out.println(name + "jest mezczyzna");
+                break;
+            }
+        }
+
+
     }
 }
