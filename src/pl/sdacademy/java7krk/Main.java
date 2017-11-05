f92fd15ded0ff5b5441947952cdcd7a924703cf2
Aneta <anetaryznar@gmail.com>
2017-11-05 15:08:05 +0100

    wersja 2

--- src/pl/sdacademy/java7krk/Main.java
+++ src/pl/sdacademy/java7krk/Main.java
@@ -5,13 +5,32 @@ import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) {
 
+        //zadanie1();
+        zadania2();
 
+
+    }
+
+
+    public static void zadania2(){
+
+
+
+
+    }
+
+    public static void zadanie1() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Jak masz na imie");
         String name = scanner.nextLine();
+        while (name.isEmpty()) {
+
+
+        }
         String[] names = new String[name.length()];
 
 
@@ -26,5 +45,26 @@ public class Main {
         }
 
 
+        /* lub
+        System.out.println("jak masz na imie");
+        String name = scanner.nextLine();
+        while (!name.equals("q")){
+            if(name.endsWith("a")){
+                System.out.println(name + "jest kobieta");
+            }else{
+                System.out.println(name+ "jest mezczyzna");
+            }
+            System.out.println("jak masz na imie");
+            name = scanner.nextLine();
+        }
+        */
+
+
+
+
+
+
+
     }
+
 }
